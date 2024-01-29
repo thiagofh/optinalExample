d3716cfb15037b2108cbcdc1c14718b1ccc17a45
thiagofh <thiagofh@protonmail.com>
2024-01-29 09:28:38 -0300

    Primeiro Commit

---
+++ src/main/java/com/exampleOptional/demoOptional/DemoOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.exampleOptional.demoOptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOptionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleOptional/demoOptional/DemoOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleOptional.demoOptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOptionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
